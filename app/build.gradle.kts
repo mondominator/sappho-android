d9dc8ce1853968b0b5b6f40cebe6ff7fbfcca40b
Mondo Diaz <mondo.m.diaz@gmail.com>
2026-02-01 20:00:44 -0600

    ci: bump version to 0.7.4 and switch to internal testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.sappho.audiobooks"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.7.3"
+        versionCode = 11
+        versionName = "0.7.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
